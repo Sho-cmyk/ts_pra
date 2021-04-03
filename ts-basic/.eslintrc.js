f028edb89d7a3c674b00286d90d28adf7ab3ed7a
Shunnosuke Fujii <shunnosukefujii@fujiishunnosukenoMacBook-Pro-2.local>
2021-04-03 17:58:22 +0900

    shuusei

---
+++ ts-basic/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "prettier",
+        "prettier/@typescript-eslint"
+    ],
+    plugins: [
+        "@typescript-eslint/eslint-plugin"
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        "sourceType": "module",
+        "project": "./tsconfig.json"
+    },
+    root: true,
+    rules: {
+
+    }
+}
--- ts-basic/src/index.ts
+++ ts-basic/src/index.ts
@@ -1,5 +1,11 @@
-import {hoge} from "./hoge"
+// import {hoge} from "./hoge"
 
-const num = 1
-console.log(hoge)
-console.log(num)
+// const num = 1
+// console.log(hoge)
+// console.log(num)
+
+import World from './world';
+
+const root = document.getElementById("root")
+const world = new World("Hello World!");
+world.sayHello(root);
